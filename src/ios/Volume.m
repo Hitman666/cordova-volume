31190d38963a67b6a0b70db3f187b976c5dc352f
Nikola Breznjak <nikola.breznjak@gmail.com>
2016-03-28 10:01:02 +0200

    Added the isMuted check for iOS

--- src/ios/Volume.m
+++ src/ios/Volume.m
@@ -41,8 +41,39 @@
 - (CDVPluginResult*) currentVolume {
   AVAudioSession* audioSession = [AVAudioSession sharedInstance];
   float volume = audioSession.outputVolume;
+  
+  boolean _isMuted = [self isMuted];
+  if (_isMuted)
+    volume = 0;
 
   return [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:volume];
+}
+
+- (boolean)isMuted{
+    bool isMuted = false;
+
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0") && ([[AVAudioSession sharedInstance] outputVolume] < 0.1)) {
+        isMuted = true;
+    }
+    else{
+      MPVolumeView *slide = [MPVolumeView new];
+      UISlider *volumeViewSlider = nil;
+
+      for (UIView *view in [slide subviews]){
+        if ([[[view class] description] isEqualToString:@"MPVolumeSlider"]) {
+          volumeViewSlider = (UISlider *) view;
+          break; 
+        }
+      }
+
+      if (volumeViewSlider) {
+        if([volumeViewSlider value] < 0.1) {
+          isMuted = true;
+        }
+      }
+  }
+
+  return isMuted;
 }
 
 @end
